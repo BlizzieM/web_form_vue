303181cdf5362b56aa26c78a09db551c6f642d64
BlizzieM <tuisku.penttila@outlook.com>
2024-04-10 13:01:21 +0300

    stufff

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Singup_form />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Singup_form from './components/Singup_form.vue'
+import {ref} from "vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Singup_form
+  },
+
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #eee;
+  margin: 0px;
 }
 </style>
