5876f49f490d54725cfdb6ecb93bb55c0e38a02c
BlizzieM <tuisku.penttila@outlook.com>
2024-04-15 13:04:48 +0300

    arrays and checkboxes

--- src/components/Singup_form.vue
+++ src/components/Singup_form.vue
@@ -5,7 +5,10 @@ export default {
   data() {
     return {
       email: ref(''),
-      password: ref('')
+      password: ref(''),
+      role: ref(''),
+      accept: ref(false),
+      aqours: ref([])
     }
   },
 }
@@ -18,12 +21,51 @@ export default {
     <input type="email" required v-model="email">
     <label>Password</label>
     <input type="password" required v-model="password">
-  </form>
+    <label>Role</label>
+    <select v-model="role">
+      <option>Web Developer</option>
+      <option>Web Designer</option>
+      <option>Project Manager</option>
+      <option>Other</option>
+    </select>
+    <p>
+    <input type="checkbox" v-model="accept"/>
+    <label>Accept terms and conditions</label>
+    </p>
+
+    <p>Choose your favorite Aqours member:</p>
+    <p>
+    <input type="checkbox" value="Chika" v-model="aqours">
+    <label>Chika</label>
+    <input type="checkbox" value="Riko" v-model="aqours">
+    <label>Riko</label>
+    <input type="checkbox" value="Kanan" v-model="aqours">
+    <label>Kanan</label>
+    <input type="checkbox" value="Dia" v-model="aqours">
+    <label>Dia</label>
+    <input type="checkbox" value="You" v-model="aqours">
+    <label>You</label>
+    <input type="checkbox" value="Yoshiko" v-model="aqours">
+    <label>Yoshiko</label>
+    <input type="checkbox" value="Hanamaru" v-model="aqours">
+    <label>Hanamaru</label>
+    <input type="checkbox" value="Mari" v-model="aqours">
+    <label>Mari</label>
+    <input type="checkbox" value="Ruby" v-model="aqours">
+    <label>Ruby</label>
+
+    </p>
+
   <p>Email: {{email}}</p>
   <p>Password: {{password}}</p>
+  <p>Role: {{role}}</p>
+  <p>Accept: {{accept}}</p>
+  <p>Aqours: {{aqours}}</p>
+  </form>
 </template>
 
 <style>
+
 form{
   max-width: 420px;
   margin: 30px auto;
@@ -31,7 +73,8 @@ form{
   text-align: left;
   padding: 40px;
   border-radius: 10px;
-  font-family: sans-serif;
+  font-family: 'Roboto', sans-serif;
+
 }
 label{
   color: #aaa;
@@ -42,7 +85,7 @@ label{
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input, select {
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -50,6 +93,13 @@ input {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 
 </style>
